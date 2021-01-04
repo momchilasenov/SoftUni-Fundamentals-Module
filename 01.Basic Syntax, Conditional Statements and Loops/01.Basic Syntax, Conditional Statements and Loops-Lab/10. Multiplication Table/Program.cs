bfbd567dd92c90c44bcf0074b0104ec4d0fa9549
momchilasenov <momchil276@gmail.com>
2021-01-04 21:53:07 +0200

    Added problem solution

---
+++ 01.Basic Syntax, Conditional Statements and Loops/01.Basic Syntax, Conditional Statements and Loops-Lab/10. Multiplication Table/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Решение с FOR цикъл
+            //int number = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int result = number * i;
+            //    Console.WriteLine($"{number} X {i} = {result}");
+            //}
+
+            //Решение с While цикъл
+            int number = int.Parse(Console.ReadLine());
+            int multiplier = 1;
+
+            while (multiplier <= 10)
+            {
+                int result = number * multiplier;
+                Console.WriteLine($"{number} X {multiplier} = {result}");
+                multiplier++;
+            }
+
+        }
+    }
+}
