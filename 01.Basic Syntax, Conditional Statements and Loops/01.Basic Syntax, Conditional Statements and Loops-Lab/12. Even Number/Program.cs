c90f07bbad5dc9d85173f54f1327b5731912f74c
momchilasenov <momchil276@gmail.com>
2021-01-04 21:54:03 +0200

    Added problem solution

---
+++ 01.Basic Syntax, Conditional Statements and Loops/01.Basic Syntax, Conditional Statements and Loops-Lab/12. Even Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12._Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (number % 2 != 0)
+                {
+                    Console.WriteLine("Please write an even number.");
+                }
+                else
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(number)}");
+                    break;
+                }
+                number = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
