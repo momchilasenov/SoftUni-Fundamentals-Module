01b2d35a2bb6509b3f95265ad7eb6407505920c3
momchilasenov <momchil276@gmail.com>
2021-01-04 21:53:34 +0200

    Added problem solution

---
+++ 01.Basic Syntax, Conditional Statements and Loops/01.Basic Syntax, Conditional Statements and Loops-Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = int.Parse(Console.ReadLine());
+            //int multiplier = int.Parse(Console.ReadLine());
+
+            //for (int i=multiplier; i<=10; i++)
+            //{
+            //    int result = number * i;
+            //    Console.WriteLine($"{number} X {i} = {result}");
+            //}
+            //if (multiplier > 10)
+            //{
+            //    Console.WriteLine($"{number} X {multiplier} = {number*multiplier}");
+            //}
+
+            //Решение с While цикъл
+            //int number = int.Parse(Console.ReadLine());
+            //int multiplier = int.Parse(Console.ReadLine());
+
+            //if (multiplier > 10)
+            //{
+            //    Console.WriteLine($"{number} X {multiplier} = {number * multiplier}");
+            //    return;
+            //}
+
+            //while (multiplier <= 10)
+            //{
+            //    int result = number * multiplier;
+            //    Console.WriteLine($"{number} X {multiplier} = {result}");
+            //    multiplier++;
+            //}
+
+            //Решение с DO...While цикъл
+            int number = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            do
+            {
+                int result = number * multiplier;
+                Console.WriteLine($"{number} X {multiplier} = {result}");
+                multiplier++;
+            } while (multiplier <= 10);
+
+
+        }
+    }
+}
