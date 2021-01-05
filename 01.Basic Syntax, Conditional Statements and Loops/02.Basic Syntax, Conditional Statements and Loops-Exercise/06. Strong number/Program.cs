a4b3d8952b9115fe095d1566e595c87613886cf5
momchilasenov <momchil276@gmail.com>
2021-01-05 18:23:43 +0200

    Added new problem

---
+++ 01.Basic Syntax, Conditional Statements and Loops/02.Basic Syntax, Conditional Statements and Loops-Exercise/06. Strong number/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // algorithm
+            // 145 % 10 = 5
+            // 145 - 5 = 140 / 10 = 14
+            // 14 % 10 = 4
+            // 14 - 4 = 10 / 10 = 1
+            // 1 % 10 = 1
+
+            int num = int.Parse(Console.ReadLine());
+            int tempNum = num;
+            string strNum = "";
+            strNum += num; // "145"
+            int sum = 0;
+
+            for (int i = 0; i < strNum.Length; i++)
+            {
+                int currentDigit = tempNum % 10;
+                tempNum = (tempNum - currentDigit) / 10;
+
+                int factorial = 1; //искаш за всяка цифра факториелът да е нов  
+
+                for (int j = 1; j<=currentDigit; j++)
+                {
+                    factorial *= j;
+                }
+                sum += factorial;
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
