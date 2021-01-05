e9b9316375ab0f637ca0fa1072dad4cce66354d2
momchilasenov <momchil276@gmail.com>
2021-01-05 18:23:00 +0200

    Added problem solution

---
+++ 01.Basic Syntax, Conditional Statements and Loops/02.Basic Syntax, Conditional Statements and Loops-Exercise/05. Login/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine(); // baby
+            string password = Console.ReadLine();
+            string reversedUsername = "";
+            int counter = 0;
+
+            for (int i = username.Length - 1; i>=0; i--)
+            {
+                reversedUsername += username[i];
+            }
+
+            while (true)
+            {
+                if (password != reversedUsername)
+                {
+                    counter++;
+                    if (counter == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break; ;
+                    }
+
+                    Console.WriteLine("Incorrect password. Try again.");
+                    password = "";
+
+                   
+                }
+                else if (password == reversedUsername)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+
+                password = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
