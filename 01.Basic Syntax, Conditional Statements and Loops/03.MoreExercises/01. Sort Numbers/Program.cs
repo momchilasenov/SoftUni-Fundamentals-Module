3b9d65ed8c59e686c951393de845242e6dd99040
momchilasenov <momchil276@gmail.com>
2021-01-07 18:33:44 +0200

    Added new problem

---
+++ 01.Basic Syntax, Conditional Statements and Loops/03.MoreExercises/01. Sort Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace More_Exercises___01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int largest = Math.Max(Math.Max(n1, n2), n3);
+
+            if (largest == n1)
+            {
+                Console.WriteLine(n1);
+                Console.WriteLine(Math.Max(n2,n3));
+                Console.WriteLine(Math.Min(n2,n3));
+            }
+            else if (largest == n2)
+            {
+                Console.WriteLine(n2);
+                Console.WriteLine(Math.Max(n1, n3));
+                Console.WriteLine(Math.Min(n1, n3));
+            }
+            else if (largest == n3)
+            {
+                Console.WriteLine(n3);
+                Console.WriteLine(Math.Max(n2, n3));
+                Console.WriteLine(Math.Min(n2, n3));
+            }
+        }
+    }
+}
