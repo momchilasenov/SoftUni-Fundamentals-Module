32ff7e17f082aeca1bc32d582bbfc9142fe3b711
momchilasenov <momchil276@gmail.com>
2021-01-07 18:37:16 +0200

    Added new problem

---
+++ 01.Basic Syntax, Conditional Statements and Loops/03.MoreExercises/04. Reverse String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace More_Exercises___04._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello -> olleH
+
+            string word = Console.ReadLine();
+
+            int length = word.Length;
+            string reversed = "";
+
+            for (int i=length-1; i>=0; i--)
+            {
+                reversed += word[i].ToString();
+
+            }
+            Console.WriteLine(reversed);
+        }
+    }
+}
