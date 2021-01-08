c3ecfa531898c4c614ecf3f482de6398f18c6137
momchilasenov <momchil276@gmail.com>
2021-01-08 18:31:14 +0200

    Added new problem

---
+++ 02.DataTypesAndVariables/01.DataTypesAndVariables-Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            decimal sum = 0.0M;
+
+            for (int i= 0; i<numbersCount; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
