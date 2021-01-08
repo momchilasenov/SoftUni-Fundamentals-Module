784577f9d968be1b7a22a93fc96a1b892e4bc0d0
momchilasenov <momchil276@gmail.com>
2021-01-08 18:30:44 +0200

    Added new solution

---
+++ 02.DataTypesAndVariables/01.DataTypesAndVariables-Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.31
+            int pounds = int.Parse(Console.ReadLine());
+            decimal dollars = pounds * 1.31M;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
