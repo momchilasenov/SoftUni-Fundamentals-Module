2974a69f1611a3baa6068e15689746059c1b2281
momchilasenov <momchil276@gmail.com>
2021-01-09 15:44:57 +0200

    Added new problem solution

---
+++ 02.DataTypesAndVariables/01.DataTypesAndVariables-Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            int asciiValue = (int)letter;
+            if (asciiValue >= 65 && asciiValue <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (asciiValue>= 97 && asciiValue<= 122)
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
