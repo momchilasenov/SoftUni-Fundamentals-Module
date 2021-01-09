1d07284bfb3b8681f41b24cec52a1c91653de014
momchilasenov <momchil276@gmail.com>
2021-01-09 15:32:15 +0200

    Added new problem

---
+++ 02.DataTypesAndVariables/01.DataTypesAndVariables-Lab/07. Concat Names/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07._Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOne = Console.ReadLine();
+            string nameTwo = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.Write($"{nameOne}{delimiter}{nameTwo}");
+
+        }
+    }
+}
