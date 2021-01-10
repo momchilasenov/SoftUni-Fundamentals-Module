19f99ee536a5767566ba7307b097833a71a97db3
momchilasenov <momchil276@gmail.com>
2021-01-10 21:48:17 +0200

    Added data types and variables exercise

---
+++ 02.DataTypesAndVariables/02.DataTypesAndVariables-Exercise/01. Integer Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber;
+            int result = (sum / thirdNumber)*fourthNumber;
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
