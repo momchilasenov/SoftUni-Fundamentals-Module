0037a387e205b3bec90ca19308592c4d26a5a40c
momchilasenov <momchil276@gmail.com>
2021-01-13 21:39:30 +0200

    Added new problem

---
+++ 02.DataTypesAndVariables/02.DataTypesAndVariables-Exercise/03. Elevator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courseCount = (int)Math.Ceiling(numberOfPeople/(double)capacity);
+
+            Console.WriteLine(courseCount);
+
+            char symbol = '1';
+            int number = symbol - '0'; //връща 1 
+
+
+
+
+        }
+    }
+}
