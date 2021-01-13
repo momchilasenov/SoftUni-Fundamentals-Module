eae13260252794d4fd2bb7b285fd493ee4ac184b
momchilasenov <momchil276@gmail.com>
2021-01-13 21:42:25 +0200

    Added new problem

---
+++ 02.DataTypesAndVariables/02.DataTypesAndVariables-Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int waterInTank = 0;
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                sum += liters;
+
+                if (sum <= 255)
+                {
+                    waterInTank += liters;
+                }
+                else if (sum > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sum -= liters;
+                }
+            }
+
+            Console.WriteLine(waterInTank);
+        }
+    }
+}
