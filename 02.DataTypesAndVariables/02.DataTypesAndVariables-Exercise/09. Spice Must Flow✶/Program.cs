00e9a049d65546f1a9e0a6dcbb4c031240bbc178
momchilasenov <momchil276@gmail.com>
2021-01-16 17:33:41 +0200

    Added new problem solution

---
+++ 02.DataTypesAndVariables/02.DataTypesAndVariables-Exercise/09. Spice Must Flow✶/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sourceYield = int.Parse(Console.ReadLine());
+            int extractedSpice = 0;
+            int dayCounter = 0;
+
+            while (sourceYield >= 100)
+            {
+                extractedSpice += sourceYield;
+                extractedSpice -= 26;
+                sourceYield -= 10;
+                dayCounter++;
+
+                if (extractedSpice <= 0)
+                {
+                    extractedSpice = 0;
+                }
+
+                if (sourceYield < 100)
+                {
+                    extractedSpice -= 26;
+                    break;
+                }
+            }
+            Console.WriteLine(dayCounter);
+            Console.WriteLine(extractedSpice);
+        }
+    }
+}
