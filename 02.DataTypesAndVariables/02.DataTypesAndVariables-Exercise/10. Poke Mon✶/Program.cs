e9d0f15f70672454303a8e72b9413fa09f18b98b
momchilasenov <momchil276@gmail.com>
2021-01-16 17:34:10 +0200

    Added new problem

---
+++ 02.DataTypesAndVariables/02.DataTypesAndVariables-Exercise/10. Poke Mon✶/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _10._Poke_Mon_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //pokePower of the pokemon
+            int m = int.Parse(Console.ReadLine()); //distance between targets
+            int y = int.Parse(Console.ReadLine()); //exhaustionFactor
+
+            int originalValueOfN = n;
+            int pokedTargets = 0;
+
+            while (n >= m)
+            {
+                pokedTargets++;
+                n -= m; //substract m from n until n<m
+
+                if (n == 0.5 * originalValueOfN)
+                {
+                    if (y != 0)
+                    {
+                        n = n / y;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(pokedTargets);
+        }
+    }
+}
