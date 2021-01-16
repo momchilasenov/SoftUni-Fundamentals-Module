ab1795a3d2570a6633875fd3cb7ac3c6044c17ba
momchilasenov <momchil276@gmail.com>
2021-01-16 17:39:02 +0200

    Added new problem solution

---
+++ 02.DataTypesAndVariables/03.DataTypesAndVariables-MoreExercises/05. Decrypting Message/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ME___05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+
+            for (int i=0; i<n; i++)
+            {
+                char letter = char.Parse(Console.ReadLine()); //R
+
+                int decryptedLetter = (int)letter + key; //136
+                message += (char)decryptedLetter;
+            }
+            Console.WriteLine(message);
+
+
+        }
+    }
+}
