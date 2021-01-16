c3133e5a17d77ae2fb532ed81d72e65c71826734
momchilasenov <momchil276@gmail.com>
2021-01-16 17:38:16 +0200

    Added new problem solution

---
+++ 02.DataTypesAndVariables/03.DataTypesAndVariables-MoreExercises/03. Floating Equality/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberOne = double.Parse(Console.ReadLine());
+            double numberTwo = double.Parse(Console.ReadLine());
+
+            double eps = 0.000001;
+
+            if ((numberOne - numberTwo < eps) && (numberTwo - numberOne < eps))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
