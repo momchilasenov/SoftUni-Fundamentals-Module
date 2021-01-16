94d98a4b2248a93c281ae69e10b664bf73af54c2
momchilasenov <momchil276@gmail.com>
2021-01-16 17:32:31 +0200

    Added new problem

---
+++ 02.DataTypesAndVariables/02.DataTypesAndVariables-Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxVolume = 0;
+            string biggestModel = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radius, 2) * height; //π * r ^ 2 * h
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    biggestModel = model;
+                }
+            }
+            Console.WriteLine(biggestModel);
+        }
+    }
+}
