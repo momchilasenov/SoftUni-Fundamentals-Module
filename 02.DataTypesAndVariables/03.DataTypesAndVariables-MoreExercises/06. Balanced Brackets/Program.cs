737f1326eeaa346e3bc0cf9cd49af13c9d743bf1
momchilasenov <momchil276@gmail.com>
2021-01-16 17:39:38 +0200

    Added final problem for data types more exercises

---
+++ 02.DataTypesAndVariables/03.DataTypesAndVariables-MoreExercises/06. Balanced Brackets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace More_Exercises___06._Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool isUnbalanced = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                counter++;
+
+                if (counter % 2 != 0)
+                {
+                    if (input != "(" && input!= ")")
+                    {
+                        isUnbalanced = true;
+                    }
+                }
+            }
+            if (isUnbalanced)
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+            else
+            {
+                Console.WriteLine("BALANCED");
+            }
+
+        }
+    }
+}
