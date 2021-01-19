97c875a23da960498d94701daeed5cc6fb2b6569
momchilasenov <momchil276@gmail.com>
2021-01-19 23:09:20 +0200

    Added new problem

---
+++ 03.Arrays/01.Arrays-Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < numbersArr.Length; i++)
+            {
+                if (numbersArr[i] % 2 == 0)
+                {
+                    evenSum += numbersArr[i];
+                }
+                else
+                {
+                    oddSum += numbersArr[i];
+                }
+            }
+            Console.WriteLine(evenSum-oddSum);
+        }
+    }
+}
