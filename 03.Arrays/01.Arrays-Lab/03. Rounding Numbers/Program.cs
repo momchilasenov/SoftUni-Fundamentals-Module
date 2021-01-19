33f0bfac1238292084b83c010881daf9fac2c240
momchilasenov <momchil276@gmail.com>
2021-01-19 23:06:34 +0200

    Added new problem solution

---
+++ 03.Arrays/01.Arrays-Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = Console.ReadLine();
+
+            //string[] splitted = input.Split();
+            //double[] numbers = new double[splitted.Length];
+
+            //for (int i = 0; i < splitted.Length; i++)
+            //{
+            //    numbers[i] = double.Parse(splitted[i]); 
+            //    int number = (int)Math.Round(numbers[i],MidpointRounding.AwayFromZero);
+            //    if (numbers[i] == 0)
+            //    {
+            //        numbers[i] = 0;
+            //    }
+            //    Console.WriteLine($"{numbers[i]} => {number}");
+            //}
+
+            //Another solution 
+
+            //double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            //for (int i=0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] == 0)
+            //    {
+            //        nums[i] = 0;
+            //    }
+            //    Console.WriteLine($"{nums[i]} => {(int)Math.Round(nums[i],MidpointRounding.AwayFromZero)}");
+            //}
+
+            //Another solution
+
+            double[] numbers = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i=0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} -> {Math.Round(numbers[i],MidpointRounding.AwayFromZero)}");
+            }
+            
+        }
+    }
+}
