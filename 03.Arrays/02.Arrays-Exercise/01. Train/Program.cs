cf01adedaead4db0990d7bcd2c91a032d17e5d2f
momchilasenov <momchil276@gmail.com>
2021-01-22 23:31:22 +0200

    Added new problem

---
+++ 03.Arrays/02.Arrays-Exercise/01. Train/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] peopleArr = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                peopleArr[i] = int.Parse(Console.ReadLine());
+                sum += peopleArr[i];
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{peopleArr[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
