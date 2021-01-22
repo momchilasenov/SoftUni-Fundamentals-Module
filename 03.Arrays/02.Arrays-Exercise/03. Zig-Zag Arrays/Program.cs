8c2fb8fb206255bf4959494964edca0c9a655ce1
momchilasenov <momchil276@gmail.com>
2021-01-22 23:32:46 +0200

    Added new problem

---
+++ 03.Arrays/02.Arrays-Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+
+            int indexZero = 0;
+            int indexOne = 1;
+
+            for (int i = 0; i < n; i++) 
+            {
+                string numbers = Console.ReadLine();
+
+                int[] numberArr = numbers.Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (i % 2 == 0) 
+                {
+                    firstArr[i] = numberArr[indexZero]; 
+                    secondArr[i] = numberArr[indexOne];
+                }
+                else
+                {
+                    firstArr[i] = numberArr[indexOne]; 
+                    secondArr[i] = numberArr[indexZero];
+                }
+            }
+
+            for (int i=0; i<n; i++)
+            {
+                Console.Write($"{firstArr[i]} ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{secondArr[i]} ");
+            }
+        }
+    }
+}
