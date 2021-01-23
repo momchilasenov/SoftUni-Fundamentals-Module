c1c390e72179e45598206f72273bb5cf4135ea63
momchilasenov <momchil276@gmail.com>
2021-01-23 21:46:53 +0200

    Added new problem

---
+++ 03.Arrays/02.Arrays-Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine() // 1 2 3 4 
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int counter = 2;
+            int givenNumber = int.Parse(Console.ReadLine()); //5
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == givenNumber)
+                    {
+                        int[] newArr = new int[counter];
+                        newArr[0] = numbers[i];
+                        newArr[1] = numbers[j];
+                        Console.WriteLine(String.Join(" ", newArr));
+                    }
+                }
+                
+            }
+
+
+        }
+    }
+}
