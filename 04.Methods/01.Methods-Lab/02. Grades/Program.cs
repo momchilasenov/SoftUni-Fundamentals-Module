fa66fb292c9f27fe012d802be1c0b43f6dc22f59
momchilasenov <momchil276@gmail.com>
2021-01-27 23:35:51 +0200

    Added new problem

---
+++ 04.Methods/01.Methods-Lab/02. Grades/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            Grades(grade);
+        }
+
+        static void Grades(double grade)
+        {
+            string score = " ";
+            if (grade>=2.00 && grade <= 2.99)
+            {
+                score = "Fail";
+            }
+            else if (grade >= 3.00 && grade <= 3.49)
+            {
+                score = "Poor";
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                score = "Good";
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                score = "Very good";
+            }
+            else if (grade >= 5.50 && grade <= 6.00)
+            {
+                score = "Excellent";
+            }
+            Console.WriteLine(score);
+        }
+    }
+}
