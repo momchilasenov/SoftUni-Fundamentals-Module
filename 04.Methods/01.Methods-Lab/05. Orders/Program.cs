70dece082afe6d2dc317447de1b3422ef2fc2c18
momchilasenov <momchil276@gmail.com>
2021-01-29 22:15:05 +0200

    Added new problem

---
+++ 04.Methods/01.Methods-Lab/05. Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string order = Console.ReadLine();
+            int productQuantity = int.Parse(Console.ReadLine());
+            TotalPriceOfOrder(order, productQuantity);
+        }
+
+        static void TotalPriceOfOrder(string order, int productQuantity)
+        {
+            if (order == "coffee")
+            {
+                Console.WriteLine($"{productQuantity * 1.50:f2}");
+            }
+            else if (order == "coke")
+            {
+                Console.WriteLine($"{productQuantity * 1.40:f2}");
+            }
+            else if (order == "water")
+            {
+                Console.WriteLine($"{productQuantity * 1.00:f2}"); 
+            }
+            else if (order == "snacks")
+            {
+                Console.WriteLine($"{productQuantity * 2.00:f2}");
+            }
+
+        }
+    }
+}
