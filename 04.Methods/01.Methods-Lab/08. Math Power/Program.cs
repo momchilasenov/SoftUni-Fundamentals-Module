afdabce8b2bd717e6b1400714ec7d44a590f408a
momchilasenov <momchil276@gmail.com>
2021-01-29 22:17:02 +0200

    Added new problem

---
+++ 04.Methods/01.Methods-Lab/08. Math Power/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double number = double.Parse(Console.ReadLine());
+            //double power = double.Parse(Console.ReadLine());
+            int number = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            Console.WriteLine(NumberToGivenPower(number,power));
+        }
+       
+        static int NumberToGivenPower(int number, int power)
+        {
+            int result = 1;
+            for (int i=0; i<power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        } 
+    }
+}
