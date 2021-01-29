20756b3c0714221b04df59a0935e00f2784f3477
momchilasenov <momchil276@gmail.com>
2021-01-29 22:18:54 +0200

    Added new problem

---
+++ 04.Methods/01.Methods-Lab/11. Math operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            char sign = char.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculator(first, sign, second));
+        }
+
+        static double Calculator (int first, char sign, int second)
+        {
+            double result = 0;
+
+            if (sign == '*')
+            {
+                result = first * second;
+            }
+            else if (sign == '/')
+            {
+                result = first / second;
+            }
+            else if (sign == '+')
+            {
+                result = first + second;
+            }
+            else if (sign == '-')
+            {
+                result = first - second;
+            }
+
+            return result;
+        }
+    }
+}
