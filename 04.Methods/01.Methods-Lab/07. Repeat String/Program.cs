dd033ebefe4c77aef10eb2f4855996aeab1d604a
momchilasenov <momchil276@gmail.com>
2021-01-29 22:16:14 +0200

    Added new problem

---
+++ 04.Methods/01.Methods-Lab/07. Repeat String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int repetitions = int.Parse(Console.ReadLine());
+            string result = RepeatedString(input, repetitions);
+            Console.WriteLine(result);
+
+        }
+
+        static string RepeatedString (string input, int repetitions)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i= 1; i<=repetitions; i++)
+            {
+                result.Append(input);
+            }
+
+            return result.ToString();
+        }
+    }
+}
