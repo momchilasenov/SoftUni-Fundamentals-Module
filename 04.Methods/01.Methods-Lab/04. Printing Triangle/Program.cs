060494e365fd363b79213a7c39697f462299fbef
momchilasenov <momchil276@gmail.com>
2021-01-29 22:13:50 +0200

    Added new problem

---
+++ 04.Methods/01.Methods-Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+
+        static void PrintLine(int n) 
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintTriangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                PrintLine(i);
+            }
+            for (int i = n - 1; i > 0; i--)
+            {
+                PrintLine(i);
+            }
+        }
+
+    }
+}
