e84f38d2dafa063191c85dd70587c7173b343995
momchilasenov <momchil276@gmail.com>
2021-02-02 23:00:10 +0200

    Added new problem

---
+++ 04.Methods/02.Methods-Exercise/P06. Middle Characters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMiddleChar(input);
+        }
+
+        static void PrintMiddleChar(string input)
+        {
+            int stringLength = input.Length;
+            int middleChar = 0;
+
+            if (stringLength % 2 != 0)
+            {
+                middleChar = (stringLength / 2);
+                Console.WriteLine(input[middleChar]);
+
+            }
+
+            else if (stringLength % 2 == 0)
+            {
+                int firstMiddleChar = stringLength / 2 - 1;
+                int secondMiddleChar = (stringLength / 2);
+                Console.WriteLine($"{input[firstMiddleChar]}{input[secondMiddleChar]}");
+            }
+
+        }
+    }
+}
