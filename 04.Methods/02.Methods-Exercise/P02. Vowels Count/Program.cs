f3a944f0f10a2bd15bd8fa6a766c7ea617f24094
momchilasenov <momchil276@gmail.com>
2021-02-02 22:57:55 +0200

    Added new problem

---
+++ 04.Methods/02.Methods-Exercise/P02. Vowels Count/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            CountVowels(text);
+        }      
+
+        static void CountVowels(string text)
+        {
+            int vowelsCounter = 0;
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' }; //масив за всички главни букви
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (vowels.Contains(text[i])) //ако в масивът vowels се съдържат гласни букви 
+                {
+                    vowelsCounter++;
+                }
+            }
+            Console.WriteLine(vowelsCounter);
+        }
+    }
+}
