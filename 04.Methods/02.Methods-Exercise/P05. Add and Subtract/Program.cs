950267e6f04b03a74977e33e1d99662f52959bd7
momchilasenov <momchil276@gmail.com>
2021-02-02 22:59:32 +0200

    Added new problem

---
+++ 04.Methods/02.Methods-Exercise/P05. Add and Subtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int result = SubtractTwoDigits(first, second, third);
+            Console.WriteLine(result);
+        }
+
+        static int SumTwoDigits(int first, int second)
+        {
+            return first + second;
+        }
+
+        static int SubtractTwoDigits(int first, int second, int third) //5
+        {
+            int sum = SumTwoDigits(first, second);
+            return sum - third;
+        }
+    }
+}
