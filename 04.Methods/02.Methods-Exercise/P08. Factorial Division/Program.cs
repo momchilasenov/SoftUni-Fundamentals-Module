5a6f07c461ffcd8b1565f6c031c4b4c4683cfaf5
momchilasenov <momchil276@gmail.com>
2021-02-02 23:07:33 +0200

    Added new problem

---
+++ 04.Methods/02.Methods-Exercise/P08. Factorial Division/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            double result = FactorialOfNumbers(first, second);
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static double FactorialOfNumbers(int first, int second)
+        {
+            double firstSum = 1;
+            for (int i = 1; i <= first; i++)
+            {
+                firstSum *= i;
+            }
+            double secondSum = 1;
+            for (int i = 1; i <= second; i++)
+            {
+                secondSum *= i;
+            }
+            double result = firstSum / secondSum;
+            return result;
+        }
+    }
+}
