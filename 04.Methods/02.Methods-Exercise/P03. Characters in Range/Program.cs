3863e7abdea3d267514c552b24dc29bb2df66e90
momchilasenov <momchil276@gmail.com>
2021-02-02 22:58:34 +0200

    Added new problem

---
+++ 04.Methods/02.Methods-Exercise/P03. Characters in Range/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            AllCharsInBetween(first, second);
+        }
+
+        static void AllCharsInBetween(char first, char second)
+        {
+            if ((int)first < (int)second)
+            {
+                for (int i = first + 1; i < second; i++)
+                {
+                    Console.Write((char)i + " ");
+                }
+            }
+            else
+            {
+                for (int i = second + 1; i < first; i++)
+                {
+                    Console.Write((char)i + " ");
+                }
+            }
+
+        }
+    }
+}
