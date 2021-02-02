f2a5533711a75a58a216af41d3306e08e8d1245f
momchilasenov <momchil276@gmail.com>
2021-02-02 23:08:10 +0200

    Added new problem

---
+++ 04.Methods/02.Methods-Exercise/P09. Palindrome Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PalindromeChecker(input);
+        }
+
+        static void PalindromeChecker(string input)
+        {
+
+            while (input != "END")
+            {
+                string reversedString = string.Empty;
+                for (int i = input.Length - 1; i >= 0; i--) //3223
+                {
+                    reversedString += input[i];
+                }
+                if (reversedString == input)
+                {
+
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
