f0815c557cf37a4dd3b41c0df4e1857e38e45904
momchilasenov <momchil276@gmail.com>
2021-02-02 22:56:57 +0200

    Added new problem

---
+++ 04.Methods/02.Methods-Exercise/P01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            Console.WriteLine(SmallestOfThreeNumbers(first, second, third));
+        }
+
+        static int SmallestOfThreeNumbers(int first, int second, int third)
+        {
+            int smallestNumber = Math.Min(third, (Math.Min(first, second)));
+            return smallestNumber;
+        }
+    }
+}
