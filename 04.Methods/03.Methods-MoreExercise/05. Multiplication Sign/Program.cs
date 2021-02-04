888b2770a22cc05dc9b50fffa6db14b8f99211f1
momchilasenov <momchil276@gmail.com>
2021-02-04 20:45:54 +0200

    Added new problem

---
+++ 04.Methods/03.Methods-MoreExercise/05. Multiplication Sign/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace More_Exercises_05._Multiplication_Sign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            if (numbers.Contains(0))
+            {
+                Console.WriteLine("zero");
+                return;
+            }
+
+            int negativeCount = 0;
+
+            foreach (int number in numbers.Where(x => x < 0))
+            {
+                negativeCount++;
+            }
+
+            if (negativeCount % 2 != 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("positive");
+            }
+        }
+    }
+}
