4a95d58e96564052130b6561ed7a2de31cc35100
momchilasenov <momchil276@gmail.com>
2021-02-06 22:11:50 +0200

    Added new problem

---
+++ 05.Lists/01.Lists-Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = ReadSingleList();
+            List<int> secondList = ReadSingleList();
+
+            Console.WriteLine(String.Join(' ', MergeLists(firstList, secondList)));
+
+        }
+
+        static List<int> ReadSingleList()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            return numbers;
+        }
+
+        static List<int> MergeLists(List<int> first, List<int> second)
+        {
+            int count = Math.Max(first.Count, second.Count);
+
+            List<int> merged = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                if (first.Count > i)
+                {
+                    merged.Add(first[i]);
+
+                }
+                if (second.Count > i)
+                {
+                    merged.Add(second[i]);
+
+                }
+            }
+
+
+            return merged;
+        }
+    }
+}
