573d99e1cfd63f5f64afb7980c626f0d5a705dc1
momchilasenov <momchil276@gmail.com>
2021-02-06 22:12:22 +0200

    Added new problem

---
+++ 05.Lists/01.Lists-Lab/04. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<string> Products = new List<string>();
+
+            for (int i = 1; i <= number; i++)
+            {
+                Products.Add(Console.ReadLine());
+            }
+
+            Products.Sort();
+
+            for (int i=1; i<=number; i++)
+            {
+                Console.WriteLine($"{i}.{Products[i-1]}");
+            }
+        }
+    }
+}
