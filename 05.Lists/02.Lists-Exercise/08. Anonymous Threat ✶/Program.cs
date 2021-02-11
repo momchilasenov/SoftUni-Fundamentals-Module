a7ed1f5368de48365c34d001c950426ebbccdc72
momchilasenov <momchil276@gmail.com>
2021-02-12 00:11:26 +0200

    Added new problem

---
+++ 05.Lists/02.Lists-Exercise/08. Anonymous Threat ✶/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08._Anonymous_Threat__own__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "3:1")
+            {
+                string[] commands = input.Split();
+                string command = commands[0];
+
+                if (command == "merge") // abc, def, ghi
+                {
+                    int startIndex = int.Parse(commands[1]);
+                    int endIndex = int.Parse(commands[2]);
+
+                    string mergedWord = string.Empty;
+
+                    if (startIndex < 0)
+                    {
+                        if (endIndex < words.Count)
+                        {
+                            startIndex = 0;
+                        }
+                    }
+                    if (endIndex >= words.Count)
+                    {
+                        if (startIndex >= 0)
+                        {
+                            endIndex = words.Count - 1;
+                        }
+                    }
+
+                    for (int i = startIndex; i <= endIndex; i++)
+                    {
+                        mergedWord += words[i];
+                    }
+                    words.RemoveRange(startIndex, endIndex - startIndex - 1);
+                    words.Insert(startIndex, mergedWord);
+                }
+                else if (command == "divide")
+                {
+                    int divide = int.Parse(commands[1]);
+                    int partitions = int.Parse(commands[2]);
+                }
+            }
+            Console.WriteLine(String.Join(' ', words));
+        }
+    }
+}
