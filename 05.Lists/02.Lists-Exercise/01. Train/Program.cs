b8dec8d635e4b64b8372aa00e080fb0411645b08
momchilasenov <momchil276@gmail.com>
2021-02-12 00:06:11 +0200

    Added new problem

---
+++ 05.Lists/02.Lists-Exercise/01. Train/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] command = input.Split();
+
+                if (command[0] == "Add")
+                {
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(command[0]); //10
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + passengers <= maxCapacity) //32+32<=75
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine(String.Join(' ', wagons));
+        }
+    }
+}
