53417efa4c060e79c0edc28abdfcc3ee90dfce84
momchilasenov <momchil276@gmail.com>
2021-02-12 00:07:16 +0200

    Added new problem

---
+++ 05.Lists/02.Lists-Exercise/03. House Party/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            List<string> people = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string input = Console.ReadLine();
+                string[] inputs = input.Split();
+
+                if (inputs[2] == "going!")
+                {
+                    if (!people.Contains(inputs[0]))
+                    {
+                        people.Add(inputs[0]);
+                    }
+                    else if (people.Contains(inputs[0]))
+                    {
+                        Console.WriteLine($"{inputs[0]} is already in the list!");
+                        continue;
+                    }
+                }
+                else if (inputs[2] == "not")
+                {
+                    if (people.Contains(inputs[0]))
+                    {
+                        people.Remove(inputs[0]);
+                    }
+                    else if (!people.Contains(inputs[0]))
+                    {
+                        Console.WriteLine($"{inputs[0]} is not in the list!");
+                        continue;
+                    }
+                }
+            }
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.WriteLine(people[i]);
+            }
+        }
+    }
+}
