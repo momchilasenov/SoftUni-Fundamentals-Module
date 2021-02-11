dd43012443b0ead7540c21b6d5af93a16051672f
momchilasenov <momchil276@gmail.com>
2021-02-12 00:10:25 +0200

    Added new problem

--- 05.Lists/02.Lists-Exercise/07. Append Arrays/Program.cs
+++ 05.Lists/02.Lists-Exercise/07. Append Arrays/Program.cs
@@ -8,30 +8,7 @@ namespace _07._Append_Arrays
     {
         static void Main(string[] args)
         {
-            ////Прочитаме лист от стрингове като сплитваме по | 
-            //List<string> numbers = Console.ReadLine()
-            //    .Split("|") //намери ли | се обособява нов елемент 
-            //    .ToList() ;
-
-            ////обръщаме листа наобратно
-            //numbers.Reverse();
-
-            ////Създаваме нов лист където да налеем елементите
-            //List<string> result = new List<string>();
-
-            ////създаваме цикъл с който да итерираме през всеки елемент и да го налеем в новия лист
-            //foreach(string currentElement in numbers)
-            //{
-            //    string[] elements = currentElement
-            //        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            //        .ToArray(); //махаме спейсовете от отделните числа
-
-            //    foreach(string element in elements) //итерираме по всеки елемент в масива от елементи
-            //    {
-            //        result.Add(element);
-            //    }
-            //}
-            //Console.WriteLine(String.Join(' ', result));
+          
 
             List<string> numbers = Console.ReadLine()
                 .Split('|')
