e867d0eac2d13603e9a6998f8ad62bd04d674931
momchilasenov <momchil276@gmail.com>
2021-02-14 20:22:33 +0200

    Added new problem

---
+++ 06.ObjectsAndClasses/01.ObjectsAndClasses-Lab/01. Date Of Week/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01._Date_Of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateString = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(dateString, "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+
+        }
+    }
+}
