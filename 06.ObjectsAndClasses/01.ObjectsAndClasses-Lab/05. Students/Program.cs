fe5ea8b3792afa733feda7af16f270d5613db2f6
momchilasenov <momchil276@gmail.com>
2021-02-14 20:29:59 +0200

    Added new problem

---
+++ 06.ObjectsAndClasses/01.ObjectsAndClasses-Lab/05. Students/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            List<Student> students = new List<Student>();
+
+            while (command != "end")
+            {
+                string[] commands = command.Split();
+
+                Student newStudent = new Student
+                {
+                    FirstName = commands[0],
+                    LastName = commands[1],
+                    Age = int.Parse(commands[2]),
+                    Hometown = commands[3]
+                };
+
+                students.Add(newStudent);
+
+                command = Console.ReadLine();
+            }
+
+            string nameOfACity = Console.ReadLine();
+
+            foreach(Student studentInfo in students)
+            {
+                if (studentInfo.Hometown == nameOfACity)
+                {
+                    Console.WriteLine($"{studentInfo.FirstName} {studentInfo.LastName} is {studentInfo.Age} years old.");
+                }
+
+            }
+
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Hometown { get; set; }
+
+    }
+}
