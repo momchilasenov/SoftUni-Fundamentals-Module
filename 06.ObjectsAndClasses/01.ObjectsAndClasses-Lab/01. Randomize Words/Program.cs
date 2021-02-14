a641156044c17c4f3efa3b8ac60ccdfbf308c9ca
momchilasenov <momchil276@gmail.com>
2021-02-14 20:28:08 +0200

    Added new problem

---
+++ 06.ObjectsAndClasses/01.ObjectsAndClasses-Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                int randomNumber = rnd.Next(0, words.Count);
+                string word = words[i];
+                words[i] = words[randomNumber];
+                words[randomNumber] = word;
+            }
+            Console.WriteLine(String.Join(Environment.NewLine, words));
+        }
+    }
+}
+
