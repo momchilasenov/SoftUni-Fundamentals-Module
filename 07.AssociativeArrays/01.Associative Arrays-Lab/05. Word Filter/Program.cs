445da28e9186540a93d5834b63bcff4fe0ca7987
momchilasenov <momchil276@gmail.com>
2021-02-18 19:35:37 +0200

    Added new problem

---
+++ 07.AssociativeArrays/01.Associative Arrays-Lab/05. Word Filter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .Select(x => x)
+                .Where(x => x.Length % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+
+            words.Select(word => word.ToLower()).OrderBy(x => x).ThenBy(x => x.Length > 4);
+
+            
+        }
+    }
+}
