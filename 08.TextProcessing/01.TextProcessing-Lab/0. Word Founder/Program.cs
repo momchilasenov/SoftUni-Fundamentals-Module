0a78947e83f236fdd3290da39db72e10b5a4bcca
momchilasenov <momchil276@gmail.com>
2021-03-04 17:30:12 +0200

    Added Text Processing lab assignments

---
+++ 08.TextProcessing/01.TextProcessing-Lab/0. Word Founder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _0._Word_Founder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sample input: 
+            //word == mom 
+            //text == mom help mom mom hey help mom
+
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int currentIndex = 0;
+
+            while (text.IndexOf(word, currentIndex) > -1) //it only finds the word you need
+            {
+                currentIndex = text.IndexOf(word, currentIndex) + 1;
+                Console.WriteLine($"{word} found at {currentIndex} position");
+            }
+        }
+    }
+}
---
+++ 08.TextProcessing/01.TextProcessing-Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = Console.ReadLine();
+
+            ////Agd#53Dfg^&4F53
+            //List<char> numbers = new List<char>();
+            //List<char> letters = new List<char>();
+            //List<char> symbols = new List<char>();
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    char currentLetter = text[i];
+
+            //    if ((int)currentLetter >= 48 && currentLetter <= 57)
+            //    {
+            //        numbers.Add(currentLetter);
+            //    }
+            //    else if ((int) currentLetter >= 65 && (int) currentLetter <= 90
+            //        || (int)currentLetter >= 97 && (int)currentLetter <= 122)
+            //    {
+            //        letters.Add(currentLetter);
+            //    }
+            //    else
+            //    {
+            //        symbols.Add(currentLetter);
+            //    }
+            //}
+            //Console.WriteLine(string.Join("", numbers));
+            //Console.WriteLine(string.Join("", letters));
+            //Console.WriteLine(string.Join("", symbols));
+
+            //доста по-елегантно решение
+            string text = Console.ReadLine();
+
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder symbols = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (char.IsDigit(text[i]))
+                {
+                    digits.Append(text[i]);
+                }
+                else if (char.IsLetter(text[i]))
+                {
+                    letters.Append(text[i]);
+                }
+                else
+                {
+                    symbols.Append(text[i]);
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(symbols);
+
+        }
+    }
+}
