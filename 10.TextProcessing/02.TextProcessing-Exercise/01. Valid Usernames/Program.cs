4a360e9e5c0bd1c1333c4dc7d1637008e89f55b8
momchilasenov <momchil276@gmail.com>
2021-03-13 13:48:25 +0200

    Added new problems

---
+++ 10.TextProcessing/02.TextProcessing-Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace _01._Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(", ", StringSplitOptions.None)
+                .ToArray();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                string currentWord = names[i];
+
+                if (currentWord.Length >= 3 && currentWord.Length <= 16)
+                {
+                    //check if each char meets the conditions
+                    for (int j = 0; j < currentWord.Length; j++)
+                    {
+                        char currentChar = currentWord[j];
+
+                        if (char.IsDigit(currentChar)
+                            ||char.IsLetter(currentChar)
+                            || currentChar == '-' 
+                            || currentChar == '_')
+                        {
+                            if (j == currentWord.Length - 1)
+                            {
+                                Console.WriteLine(currentWord);
+                            }
+                            continue;
+                            
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        
+                    }
+                }
+            }
+
+
+        }
+    }
+}
